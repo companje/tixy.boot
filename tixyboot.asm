365247c93015fdb268497f6b75c8e3b4e9b32e8b
Martijn Gerkes <zeroZshadow@gmail.com>
2025-06-03 17:18:07 +0200

    Balls

--- tixyboot.asm
+++ tixyboot.asm
@@ -1,4 +1,7 @@
 org 0
+cpu 8086
+bits 16
+default rel
 
 COLS  equ 72
 TOP   equ 9*4*COLS+20*4    ; row=9,col=20
@@ -14,220 +17,180 @@ BLUE  equ 0xf4
 
 jmp setup
 
-fx_table:      
-    db fx0,fx1,fx2,fx3,fx4,fx5,fx6,fx7
+fx_table:
+    db fx_pulse, fx_spinner, fx_spinner, fx_checker, fx_checker, fx_sphere, fx_sphere, fx_pulse
 
-fx0: ; y+t
-    mov al,t
-    ; sub al,i
-    add al,x
-    add al,y
+fx_pulse:
     ret
 
-fx1: ; x+y+t
-    mov al,x
-    add al,y
-    add al,t
-    and al,31
-    sub al,15
+fx_spinner:
+    sub x, 8
+    mul x
+    add al, y
     ret
 
-fx2: 
-    mov al,x
-    sub al,y
-    sub al,t
-    and al,31
-    sub al,15
-    ret
-
-fx3:
-    add al,t
-    and al,31
-    sub al,15
-    ret
-
-fx4: ; xor
+fx_checker:
     mov al,x
     xor al,y
     add al,t
-    and al,31
-    sub al,15
     ret
 
-fx5: ; 
-    mov al,y
-    sub al,x
-    mov cl,-8
-    mul cl
-    add al,t
-    and al,31
-    sub al,15
-    ret
-
-fx6: ; ((y-x)*-8)+t
-    mov al,y
-    sub al,x
-    mov cl,-8
-    mul cl
-    ;geen ret hier
-
-
-
-fx7:
+fx_sphere:
     mov al,x
-    ; mov cl,y
     inc al
     mul y
     add al,t
-    and al,31
-    sub al,15
     ret
 
 setup:                      ; starting point of code
-
-
-; generate_chars:
+                            ; generate_chars
     push cs
     pop ds                  ; ds:si in code segment
     push cs
     pop es                  ; es:di in code segment
 
+    ; We're building 16 characters, of which 8 are copies, and 2 empty
+    ; 1st character is pre-made, 8th character is empty
+    ; generate 7 characters, then copy and flip
 
-    mov di,bitmap_data
-    mov cx,16*4*4
-    mov ax,-1
-.lp:
-    test cx,3
-    jnz .sk
-    ; xchg ah,al
-    ; mov ax,0x5555
-    shr ax,1
-.sk:
-    stosw
-    stosw
-    loop .lp
+    mov cl, 7
+    mov si, bitmap_data         ; original character
+    mov di, bitmap_data + (4*4) ; new character
+.character:
+    push cx
+    mov cl, 8       ; copy 8 16bit values, characters are 4 * 4 bytes
+    rep movsw
 
+    push si         ; We've copied the previous character, si now points at our new character
+    mov cl, 2
+.shrink_half:       ;shrink top and bottom half of character
+    push cx
+    mov cl, 4
+.stripe:            ; shrink even and uneven lines
+    shr byte [si+0], 1
+    shl byte [si+4], 1
+    inc si
+    loop .stripe
+    pop cx
 
+    add si, 4
+    loop .shrink_half
 
+    pop si
+    pop cx
+    loop .character
 
+    mov cl, 8
+.reverse:       ; copy characters in reverse
+    push cx
+    mov cl, 8
+    rep movsw
+    sub si, 16*2
+    pop cx
+    loop .reverse
 
+    ; AL is still 0 here, use it to make an empty character
+    mov cl, 16
+    rep stosb
+
+init:
     xor bp,bp
     xor dx,dx               ; t=i=0 (clear time and index)
 
 draw:
-    and bp,7
+    and bp,7                ; wrap fx index
+    push bp
+    mov bl,[byte ds:bp+fx_table] ;change bp from index to address
+    xor bh,bh
+    push bx
+    pop bp
+
     mov di,TOP              ; left top corner to center tixy
 dot:
     mov al,i                ; al=index
+    mov bl,t
     xor ah,ah               ; ah=0
     mov cl,16
     div cl                  ; calculate x and y from i
-    ; xchg ax,bx              ; bh=x, bl=y
-    mov bx,ax
+    xchg bx,ax               ; bh = x : bl = y, al = t
 
-  .cont:
-   
-    push bp
+
     push bx
-    xchg bx,bp
-    mov bl,[bx+fx_table]
-    xor bh,bh
-    xchg bx,bp
-    pop bx
+    ; cx is free, ah = 0, al = t, bx used for X and Y
     call bp                 ; call the effect function
-    
+    pop bx
+    and al,15 ;maybe 31?
 
-    ;more red!
-    ; and al,31
-    ; sub al,15
-
-    ; out 0x3a,al
-    
-    pop bp
- 
 draw_char_color:
-    cmp al,0
+    cmp al, 8 ; maybe 15?
     pushf
-    jge .red
-    neg al
   .red:
     mov cl, 4
-    and al,15               ; limit al to 15
-    cbw                     ; ah=0
-    shl al,cl               ; al*=16
+    and al,15           ; limit al to 15
+    cbw                 ; ah=0
+    shl al,cl           ; al*=16
     add ax,bitmap_data
+    mov cl, 0
 
-    mov cx,RED << 8              ; ch=0xf0, cl=0
+    mov ch, RED
     call draw_char
 
     popf
-    jge .green_blue
-    ; xor al,al               ; if negative then just red so clear (al=0) green and blue
-    mov ax,bitmap_data ; this is the same as al=0 (empty character)
+    jge .blue_green
+    mov ax, bitmap_data + 16 * 16  ; swap to an empty character so green and blue are cleared, leaving only red
 
-  .green_blue:
-    mov cx,GREEN<<8
+  .blue_green:
+    mov ch, BLUE
     call draw_char
-    mov cx,BLUE<<8
+    mov ch, GREEN
     call draw_char
 
-    ; mov ch,GREEN
-    ; call draw_char
-    ; mov ch,BLUE
-    ; call draw_char
-    
-
-  .next:  
+  .next:
     inc i                   ; i++
 
-    add di,8         
+    add di,8
     cmp x,15
     jl dot                  ; next col
-    add di,4*COLS+160       
+    add di,4*COLS+160
     cmp y,15
     jl dot                  ; next line
     inc t
     and t,31
-    jnz draw                 ; next frame
+    pop bp                  ; change bp back to index
+    jnz draw                ; next frame
     inc bp                  ; inc effect
-  
-    jmp draw
+    jmp draw                ; next frame
 
 draw_char:                  ; es:di=vram (not increasing), al=char 0..15, destroys cx
     push di
-    ; push si
-    ; push cx
 
     push cx
     pop es                  ; es (color channel was in cx)
-    push cs
-    pop ds                  ; ds=cs
-
-    mov cx,4
-    push cx
-    push cx
 
     mov si,ax              ; si = source address of rendered bitmap char
 
-    pop cx                  ;cx=4
-    rep movsw
-    add di,4*COLS-8
-
-    pop cx                  ;cx=4
-
-    mov si,ax 
+    mov cx,4
+    push cx
     rep movsw
 
-    ; pop cx
-    ; pop si
-    pop di                    
+    add di,4*COLS-8         ; move 2 rows down, and back by 8 bytes back.
+    pop cx                  ;cx=4
+    rep movsw
+
+    pop di
     ret
 
+bitmap_data:                ; destination for 128 bytes rendered bitmap data, initial character filled
+    ;db 00000000b, 00000111b, 00011111b, 01111111b, 00000000b, 11100000b, 11111000b, 11111110b
+    ;db 01111111b, 00011111b, 00000111b, 00000000b, 11111110b, 11111000b, 11100000b, 00000000b
+    db 00000000b, 00001111b, 00111111b, 11111111b, 00000000b, 11110000b, 11111100b, 11111111b
+    db 11111111b, 00111111b, 00001111b, 00000000b, 11111111b, 11111100b, 11110000b, 00000000b
 
-bitmap_data:                          ; destination for 128 bytes rendered bitmap data
-    ; incbin "Tixy-mini-char-Sheet.spr"
 
 %assign num $-$$
 %warning total num
+%if num > 256
+    %error program too large
+%endif
 
 times (180*1024)-num db  0                 ; fill up with zeros until file size=180k
